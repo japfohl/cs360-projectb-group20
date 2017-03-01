f8587bb4a9fc53dd3613fa00719c6c8b4d64cf26
Collin James <jamesc2@oregonstate.edu>
2017-03-01 02:10:42 +0000

    tables are successfully created

--- sql/Test.sql
+++ sql/Test.sql
@@ -6,20 +6,15 @@ sudo mysql
 source Test.sql;
 */
 
-set foreign_key_checks = 0; -- force drop
-DROP TABLE IF EXISTS `products`;
-set foreign_key_checks = 1;
-
-
-CREATE TABLE IF NOT EXISTS products (
-	product_id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	photo_url VARCHAR(255) NOT NULL,
-	PRIMARY KEY (product_id),
-	CONSTRAINT uniq_name UNIQUE (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- -- test product
+
+-- SHOW TABLES LIKE 'product'; -- come back to this later
+
 INSERT INTO products(name, photo_url) 
 VALUES ('Pocky Biscuit Stick 5 Flavor Variety Pack (Pack of 5)','https://images-na.ssl-images-amazon.com/images/I/71QzLsqscXL._SX522_.jpg'),
 ('Wedderspoon 100% Raw Premium Manuka Honey KFactor 16+, 17.6 oz','https://images-na.ssl-images-amazon.com/images/I/61vlQw5CTIL._SY679_.jpg');
+
+SELECT COUNT (*) FROM products;
+
+SELECT * FROM PRODUCTS WHERE name LIKE '%Pocky%';
